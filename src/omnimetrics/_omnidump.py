d12dfe46bb8d12d308e54c629344f43bc1efa0fd
Jonathan Lange <jml@mumak.net>
2020-11-22 09:19:26 +0000

    Clarify todo list

--- src/omnimetrics/_omnidump.py
+++ src/omnimetrics/_omnidump.py
@@ -37,39 +37,39 @@ def jsonify(o):
 """
 Here's what we need to do.
 
-- use default_document to get the whole database
-- use `flattened` versions to get at the actual contents
-  - `flattened_tasks`
-  - `flattened_folders`
-  - `flattened_tags`
-  - `flattened_projects`
+- [x] use default_document to get the whole database
+- [ ] use `flattened` versions to get at the actual contents
+  - [x] `flattened_tasks`
+  - [ ] `flattened_folders`
+  - [ ] `flattened_tags`
+  - [ ] `flattened_projects`
 
 - build types for each of those flattened versions
-  - https://omni-automation.com/omnifocus/OF-API.html#Task
-  - folder
-  - tag
-  - project
+  - [x] https://omni-automation.com/omnifocus/OF-API.html#Task
+  - [ ] folder
+  - [ ] tag
+  - [ ] project
 
 - serialisers for each of these
-  - JSON
-  - Avro? https://avro.apache.org/docs/1.10.0/gettingstartedpython.html
+  - [x] JSON
+  - [ ] Avro? https://avro.apache.org/docs/1.10.0/gettingstartedpython.html
 
-- Figure out how to use launchd to run a script daily
-  - https://www.launchd.info/
-  - Put a plist file in ~/Library/LaunchAgents
+- [x] Figure out how to use launchd to run a script daily
+  - [x] https://www.launchd.info/
+  - [x] Put a plist file in ~/Library/LaunchAgents
 
-- Create a Terraform module that:
-  - creates a GCP project
-  - creates a GCS bucket
-  - creates a BigQuery dataset
+- [ ] Create a Terraform module that:
+  - [ ] creates a GCP project
+  - [x] creates a GCS bucket
+  - [ ] creates a BigQuery dataset
 
-- Create a script that uploads the serialised, flattened things to GCS
+- [ ] Create a script that uploads the serialised, flattened things to GCS
 
-- Create another script that loads those flattened things to BigQuery
+- [ ] Create another script that loads those flattened things to BigQuery
 
-- Build a dbt module that transforms the raw data into something useful
+- [ ] Build a dbt module that transforms the raw data into something useful
 
-- Build a DataStudio dashboard based on that data.
+- [ ] Build a DataStudio dashboard based on that data.
 
 Strategy
 
